f839e93bf580a75662cc0532b791dfa55b70d405
Manmohan Aeir <manmohanaeir001110@gmail.com>
2023-01-22 12:13:47 +0545

    up to patients model

---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title> Health niche</title>
+	
+    <!-- css -->
+    <link href="/assest/css/bootstrap.min.css" rel="stylesheet" type="text/css">
+    <link href="/assest/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css" />
+	<link rel="stylesheet" type="text/css" href="/assest/plugins/cubeportfolio/css/cubeportfolio.min.css">
+	<link href="//assest/css/nivo-lightbox.css" rel="stylesheet" />
+	<link href="//assest/css/nivo-lightbox-theme/default/default.css" rel="stylesheet" type="text/css" />
+	<link href="/assest/css/owl.carousel.css" rel="stylesheet" media="screen" />
+    <link href="/assest/css/owl.theme.css" rel="stylesheet" media="screen" />
+	<link href="/assest/css/animate.css" rel="stylesheet" />
+    <link href="/assest/css/style.css" rel="stylesheet">
+
+	<!-- boxed bg -->
+	<link id="bodybg" href="bodybg/bg1.css" rel="stylesheet" type="text/css" />
+	<!-- template skin -->
+	<link id="t-colors" href="color/default.css" rel="stylesheet">
+
+
+</head>
+<body id="page-top" data-spy="scroll" data-target=".navbar-custom">
+
